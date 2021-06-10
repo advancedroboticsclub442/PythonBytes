009aaa404a93103c400d0ac12a5a9fc3222fd653
Ayush Aryal <ayusharyal100@gmail.com>
2021-06-10 22:04:44 +0545

    Rasik Nepal's portion added

---
+++ day1_part1.py
@@ -0,0 +1,129 @@
+# print("hello world")
+# variable
+# container for value
+# reference to object and behaves as the object it is pointing to
+
+# string
+# name = "124@@#ram"
+# print('"it\'s ok" he said')
+# print (name + 'hari'*2)
+# print("""hello there
+# welcome to our house
+# """)
+# print("hello there \n welcome")
+# number
+# a = 10
+# b = 10.5
+# print(10/6)
+# print(10//9)  # floor division  5.99
+# print(3**3)
+# print(2.2*3**3)
+# boolean
+# is_human = False
+
+# print(27/5)
+# print(27//5)
+# # object can be int , flot,string or boolean
+# n = 300
+# print(type(n))
+# # m = 400
+# # m = n = 300
+# n = "foo"
+# print(type(n))
+# a = '20'
+# print(type(a))
+# print(type(int(a))) #str()
+# input always gives string
+# name = input("enter your name ")
+# print(type(name))
+# float +- int = float
+
+# age = int(input("enter your age "))
+# birth_year = 2021 - age
+# print("birth year is " + str(birth_year))
+
+# string
+# name = "Advanced College"
+# # slicing string[starting index : ending index : step size]
+# print(name[ -4: -2])
+# print(name)
+# print(name[1:10])
+
+# string methods
+
+# print(name.upper())
+# print(name.lower())
+# print(name)
+# print(name.capitalize())
+# num = "45"
+# name = "ramhasgask"
+# print(num.isdigit())
+# print(name.index("a"))  # returns the first index
+# print(name.replace('a','d'))
+# print('Ram' in name)
+# print(num[1])
+# num[1]= "3"
+# print(num)
+
+# immutable
+
+# list
+# ordered collection of related objects/items
+# mutable
+
+# print(type(lis))
+# num1 = [1,2,3]
+# num2 = [2,1,3]
+# print (num1 == num2)
+# print(lis[3:6])
+# new_list = [1,2, [67,87],65,[34,87,98]]
+# print(new_list.index([67,87]))
+lis = ['ram','hari','sita',12,87.56,True,'sita',12]
+
+# list methods
+# lis.append(98)  # add new object at the last index
+# lis.insert(2,'second')
+# lis.remove('hari')  # deletes the given object
+# print(lis)
+# lis.clear()  # deletes all objects
+# print(lis)
+
+num = [3,76,81,87,76,56]
+# num.sort()  # sort in ascending order
+# print(num.index(81))
+num.remove(76)
+print(num)
+
+# tuples
+# immutable
+student = ('ram', 44, "male")
+# print(type(student))
+# print(student.index(44))
+# print(student.count(44))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
